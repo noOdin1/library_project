bc55f4b6454aeb4943c9b23c141d458a7e7198d9
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-08-29 19:59:12 +0800

    Add code from previous example with an addition of uuid

--- script.js
+++ script.js
@@ -40,3 +40,34 @@ function Book(uuid, title, author, pages, status) {
 
 function addBookToLibrary() {}
 
+/* Assuming that self.crypto.randomUUID() is available */
+
+let uuid = self.crypto.randomUUID();
+console.log("Sample of uuid: " + uuid); // for example "36b8f84d-df4e-4d49-b662-bcde71a8764f"
+
+const book1 = new Book(
+  self.crypto.randomUUID(),
+  "The Lord of the Rings",
+  "Tolkien",
+  1216,
+  "read",
+);
+const book2 = new Book(
+  self.crypto.randomUUID(),
+  "The Lost Symbol",
+  "Brown",
+  624,
+  "read",
+);
+const book3 = new Book(
+  self.crypto.randomUUID(),
+  "Sword of Destiny",
+  "Sapkowski",
+  416,
+  "unread",
+);
+
+console.log("Book1 = " + book1.info());
+console.log("Book2 = " + book2.info());
+console.log("Book3 = " + book3.info());
+
