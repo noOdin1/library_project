9ba4a8c1721e6a1c9349c29ef6b4e4c40f1ea13f
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-08-29 19:58:40 +0800

    Add code from previous examples

--- script.js
+++ script.js
@@ -2,7 +2,41 @@
 
 const myLibrary = [];
 
+function Book(uuid, title, author, pages, status) {
+  if (!new.target) {
+    throw Error(
+      "[Book] Constructor error. You must use the 'new' operator to call the constructor.",
+    );
+  }
+  this.uuid = uuid;
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.status = status;
 
+  this.info = function () {
+    // console.log(
+    //   "[Book:bookInfo] title: " +
+    //     this.title +
+    //     ", author: " +
+    //     this.author +
+    //     ", pages: " +
+    //     this.pages,
+    // );
+    return (
+      "uuid: " +
+      this.uuid +
+      ", title: " +
+      this.title +
+      ", author: " +
+      this.author +
+      ", pages: " +
+      this.pages +
+      ", status: " +
+      this.status
+    );
+  };
+}
 
 function addBookToLibrary() {}
 
