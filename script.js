467c63fcc50fa081bb4c4d7db7285d87c29151b9
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-09-08 03:08:36 +0800

    Reduce redundancy in code, still testing

--- script.js
+++ script.js
@@ -150,31 +150,41 @@ function changeBookStat(event) {
   // create a handle for button with matching id
   let tmpBtn = document.getElementById(event.target.id);
 
+  let toggleVar = tmpBtn.innerHTML === "Unread" ? "Read" : "Unread";
+
+  tmpBtn.innerHTML = toggleVar;
+  // finding object in array that matches certain criteria
+  //   src: https://stackoverflow.com/questions/12462318/find-a-value-in-an-array-of-objects-in-javascript
+  myLibrary.find((book, index) => {
+    if (book.uuid == event.target.id) {
+      myLibrary[index].status = toggleVar;
+      console.log(myLibrary[index]);
+    }
+  });
+
   // Determine if the book has been 'read' or 'unread'
-  if (tmpBtn.innerHTML == "Unread") {
-    tmpBtn.innerHTML = "Read";
-
-    // finding object in array that matches certain criteria
-    //   src: https://stackoverflow.com/questions/12462318/find-a-value-in-an-array-of-objects-in-javascript
-    myLibrary.find((book, index) => {
-      if (book.uuid == event.target.id) {
-        myLibrary[index].status = "Read";
-        console.log(myLibrary[index]);
-      }
-    });
-    return;
-  }
-  if (tmpBtn.innerHTML == "Read") {
-    tmpBtn.innerHTML = "Unread";
-
-    myLibrary.find((book, index) => {
-      if (book.uuid == event.target.id) {
-        myLibrary[index].status = "Unread";
-        console.log(myLibrary[index]);
-      }
-    });
-    return;
-  }
+  // if (tmpBtn.innerHTML == "Unread") {
+  //   tmpBtn.innerHTML = "Read";
+  //
+  //   myLibrary.find((book, index) => {
+  //     if (book.uuid == event.target.id) {
+  //       myLibrary[index].status = "Read";
+  //       console.log(myLibrary[index]);
+  //     }
+  //   });
+  //   return;
+  // }
+  // if (tmpBtn.innerHTML == "Read") {
+  //   tmpBtn.innerHTML = "Unread";
+  //
+  //   myLibrary.find((book, index) => {
+  //     if (book.uuid == event.target.id) {
+  //       myLibrary[index].status = "Unread";
+  //       console.log(myLibrary[index]);
+  //     }
+  //   });
+  //   return;
+  // }
 }
 
 function removeCard(event) {
