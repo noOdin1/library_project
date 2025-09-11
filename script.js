be3a2105d45509e9f8698cfbdc584137c8f9bdbe
noOdin1 <218098565+noOdin1@users.noreply.github.com>
2025-09-11 23:18:44 +0800

    Added a new function to create HTML elements

--- script.js
+++ script.js
@@ -39,6 +39,34 @@ function addBookToLibrary(book) {
   // console.log(myLibrary);
 }
 
+function createElement(elemType, class_list, unique_id, text_content) {
+  let element = document.createElement(elemType);
+
+  /* Add class to the element */
+  if (!(class_list instanceof Array) && class_list != "") {
+    class_list = [class_list];
+  }
+  if (class_list != "") {
+    class_list.forEach((elem) => element.classList.add(elem));
+  }
+
+  /* Add id to the element */
+  if (unique_id !== "" && !(unique_id === undefined || unique_id === null)) {
+    element.setAttribute("id", unique_id);
+  }
+
+  /* Add textContent/innerHTML/innerText value */
+  if (
+    text_content !== "" &&
+    text_content !== undefined &&
+    text_content !== null
+  ) {
+    element.textContent = text_content;
+  }
+
+  return element;
+}
+
 const book1 = new Book(
   self.crypto.randomUUID(),
   "The Lord of the Rings",
